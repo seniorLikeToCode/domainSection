2477b31cfa3c1a94c2dd243c6b9970ff1f5c675f
seniorLikeToCode <abhishek234543234543@gmail.com>
2022-01-18 18:49:08 +0530

    done

--- src/Domain/Card/Card.js
+++ src/Domain/Card/Card.js
@@ -3,11 +3,19 @@ import "./Card.css";
 
 function card({ data }) {
   return (
-      <div className="DomainCard">
-        <h1> {data.domainName} </h1>
-        <img src={data.domainLogo} alt={data.domainName} width="300px" />
-        <p> {data.content}</p>
+    <div className="flip-card-container">
+      <div className="flip-card">
+        <div className="flip-card-front">
+          <h1> {data.domainName} </h1>
+          <div className="flex">
+            <img src={data.domainLogo} alt={data.domainName} />
+          </div>
+        </div>
+        <div className="flip-card-back">
+          <p> {data.content}</p>
+        </div>
       </div>
+    </div>
   );
 }
 
